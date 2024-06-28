2c45caab73dbcda0fbb3cdf68c52a3248882d1f5
shadowsnavi <kumori.k.kuro@gmail.com>
2024-06-28 15:52:54 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names,event){
+    let new_arr=[]
+    for (let i=0; i < names.length; i++){
+        new_arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return new_arr
+}
+
+function countDown(num){
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }   
+}
